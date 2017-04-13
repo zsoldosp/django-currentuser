dd88135e6f7284938cd6ae8a0f39bdb4ffed8269
Martin Bauer <martin.bauer@paessler.com>
2017-04-13 11:38:32 +0200

    Remove everything that is not crucial for tests to pass

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 
 
 urlpatterns = (
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
     url(r'^login/$', auth_views.login, name="login"),
 )
