f8b21683fc369b96802944af99bcf81722025733
Martin Bauer <martin.bauer@paessler.com>
2018-06-22 08:04:33 +0200

    Adapt url patterns to django 2.0+, use AUTH_USER_MODEL to support custom classes

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url
+import django
 from django.contrib.auth import views as auth_views
 
 
-urlpatterns = (
-    url(r'^login/$', auth_views.login, name="login"),
-)
+if django.VERSION < (2, 0):
+    from django.conf.urls import url as path
+else:
+    from django.urls import path
+
+
+urlpatterns = [
+    path(r'login/', auth_views.LoginView.as_view(), name="login")
+]
