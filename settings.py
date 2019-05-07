a1363eb9316ee806f5f79b432c71a353ad7aff93
Sebastian Bonhag <sebastian.bonhag@paessler.com>
2019-05-07 08:58:25 +0200

    Add django.contrib.messages to the list of installed applications

--- settings.py
+++ settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django_currentuser',
     'testapp',
 )
